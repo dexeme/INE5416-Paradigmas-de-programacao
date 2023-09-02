bdfc94e9b67271465a0085cf6db88dbec02151ec
dexeme <dexe.me12@gmail.com>
2023-09-02 15:29:44 -0300

    Add exercise 16 list IV

---
+++ 16/16.hs
@@ -0,0 +1,16 @@
+-- Aluno: Tiago Siqueira
+-- Matricula: 22102193
+
+-- Crie uma função que receba dois números x e y e retorne se x  ́é divisível por y. Leia x e y do teclado.
+
+main = do
+    putStrLn "Digite o valor de x: "
+    x <- getLine
+    putStrLn "Digite o valor de y: "
+    y <- getLine
+    let x' = (read x :: Int)
+    let y' = (read y :: Int)
+    print (divisivel x' y')
+
+divisivel :: Int -> Int -> Bool
+divisivel x y = if (x `mod` y) == 0 then True else False -- Se o resto da divisao de x por y for igual a 0, retorna True, senao retorna False
