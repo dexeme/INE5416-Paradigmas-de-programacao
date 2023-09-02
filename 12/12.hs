fc7af8713036ca929d8662c1983c822388ceb723
dexeme <dexe.me12@gmail.com>
2023-09-02 15:12:40 -0300

    Add exercise 12 list IV

---
+++ 12/12.hs
@@ -0,0 +1,25 @@
+-- Aluno: Tiago Siqueira
+-- Matricula: 22102193
+
+-- Crie uma função que receba três números x, y e z e retorne o máximo divisor comum (DICA: apenas
+-- modifique o algoritmo anterior). Leia x, y e z do teclado.
+
+main = do
+    putStrLn "Digite o valor de x: "
+    x <- getLine
+    putStrLn "Digite o valor de y: "
+    y <- getLine
+    putStrLn "Digite o valor de z: "
+    z <- getLine
+    let x' = (read x :: Int)
+    let y' = (read y :: Int)
+    let z' = (read z :: Int)
+    print (mdc3 x' y' z')
+
+mdc3 :: Int -> Int -> Int -> Int
+mdc3 x y z = mdc (mdc x y) z -- Retorna o mdc de x, y e z
+
+-- Algoritmo da questao 11/11.hs:
+
+mdc :: Int -> Int -> Int
+mdc x y = if y == 0 then x else mdc y (x `mod` y) -- Se y for igual a 0, retorna x, senao retorna mdc y (x mod y)
