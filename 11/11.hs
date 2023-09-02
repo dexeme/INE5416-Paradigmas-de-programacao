e2e9140655b515b2549e9b5a368504e64337a23a
dexeme <dexe.me12@gmail.com>
2023-09-02 15:48:21 -0300

    Add exercise 11 list IV

--- 11/11.hs
+++ 11/11.hs
@@ -1,7 +1,7 @@
 -- Aluno: Tiago Siqueira
 -- Matricula: 22102193
 
--- Crie uma função que receba dois números x e y e retorne o máximo divisor comum (DICA: pesquise sobre
+-- 11. Crie uma função que receba dois números x e y e retorne o máximo divisor comum (DICA: pesquise sobre
 -- o Algoritmo de Euclides). Leia x e y do teclado
 
 main = do
